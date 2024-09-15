86c4d248c4cd80b3285583298d6df291d7f8a761
yahirdpr16 <depazreyesangel@gmail.com>
2024-09-14 23:12:55 -0600

    Creación de componente TareaListComponent para listar tareas

---
+++ Frontend/gestor-tareas/src/app/tarea-list/tarea-list.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import {Tarea} from '../models/tarea';
+import { TareasService } from '../services/tareas.service';
+
+@Component({
+  selector: 'app-tarea-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './tarea-list.component.html',
+  styleUrl: './tarea-list.component.css'
+})
+export class TareaListComponent implements OnInit{
+tareas: Tarea[]=[];
+constructor(private tareasService:TareasService){}
+
+ngOnInit(): void {
+  this.getTareas();
+}
+
+getTareas():void{
+  this.tareasService.getTareas().subscribe(
+    (data:Tarea[])=>{
+      this.tareas=data;
+    },
+    (error)=>{
+      console.error('Error al obtener tareas',error);
+    }
+  );
+}
+
+actualizarEstado(tarea: Tarea):void{
+  tarea.estado=tarea.estado==='En progreso'?'Completada':'En progreso';
+  this.tareasService.actualizarTarea(tarea.id,tarea).subscribe(
+    (tareaActualizada)=>{
+      console.log('Estado de la tarea actualizado', tareaActualizada);
+    },
+    (error)=>{
+      console.log("Error al actualizar la tarea",error);
+    }
+  );
+}
+}
